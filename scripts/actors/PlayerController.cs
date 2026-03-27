b3254f9baa89606fcb53d32965690c9d1a84182b
egofree <philippedepol@gmail.com>
2026-03-27 21:52:04 +0100

    Add player sprite, animations and PlayerController setup

--- scripts/actors/PlayerController.cs
+++ scripts/actors/PlayerController.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class PlayerController : Node
 {
+	private AnimatedSprite2D _animatedSprite;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedSprite.Play("move_right");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
